f688b60e2f7047660d6c998ec12ce47a50219b7d
廖晓卫 <liaoxiaowei@my7g.cn>
2020-03-30 11:03:33 +0800

    修复手动切换不了自动问题

--- lua-func/cmd-func/cmd_handle.lua
+++ lua-func/cmd-func/cmd_handle.lua
@@ -105,7 +105,7 @@ local function update_method()
 		local update_json = {}
 		update_json["auto_mode"] = 1
 		update_json["online"] = 1
-		g_sql_app.query_dev_info_tbl(json_body["DevId"])
+		result = g_sql_app.query_dev_info_tbl(json_body["DevId"])
 		if result[1]["dev_type"] == json_body["DevType"] then
 			g_sql_app.update_dev_status_tbl(json_body["DevId"],update_json)
 			g_cmd_sync.cmd_start_stop_rule(json_body["DevType"],json_body["DevId"], 0)
