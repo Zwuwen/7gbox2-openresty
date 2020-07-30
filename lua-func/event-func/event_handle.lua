d9a2818c9779aef7d2f66123f24525ad8e2c7829
李鹏飞 <lipengfei@my7g.cn>
2020-07-30 13:37:51 +0800

    网关收到的状态改为异步处理

--- lua-func/event-func/event_handle.lua
+++ lua-func/event-func/event_handle.lua
@@ -6,81 +6,25 @@
 --load module
 local cjson = require("cjson")
 local g_mydef = require("common.mydef.mydef_func")
-local g_event_report = require("event-func.event_report")
+local g_event_handle = require("event-func.event_handle_model")
 local g_sql_app = require("common.sql.g_orm_info_M")
 
-
-------------------------------------event handle function api-----------------------------------------
-local function get_method()
-    
-end
-
-local function put_method()
-    
-end
-
-local function delete_method()
-    
-end
-
--------------post method---------
-local function post_method()
-    ngx.req.read_body()
-    local request_body = ngx.req.get_body_data()
-    ngx.log(ngx.DEBUG,"recv post event: ",request_body)
-    local body = cjson.decode(request_body)
-    if body["Event"] == "ThingsOnline" then
-        --设备上线事件
-        g_event_report.thing_online(body["Devices"])
-    elseif body["Event"] == "ThingsOffline" then
-        --设备下线事件
-        g_event_report.thing_offline(body["Devices"])
-    elseif body["Event"] == "StatusUpload" then
-        --设备属性改变
-        g_event_report.attribute_change(body)
-    elseif body["Event"] == "ResultUpload" then
-        --命令操作结果反馈
-        g_event_report.method_respone(body)
-    elseif body["Event"] == "LinkageRuleStatus" then
-        --联动事件
-        g_event_report.linkage_event(body)
-    elseif body["Event"] == "PlatStatus" then
-        --平台上线下线事件
-        g_event_report.platform_online_event(body)
-    elseif body["Event"] == "Alarm" then
-        --AI事件
-        g_event_report.attribute_change(body)
-    elseif body["Event"] == "TimerRuleStatus" then
-        --时间策略执行状态
-        g_event_report.transmit_do_nothing(body)
-    else
-        local json_str = '{\n\"Code\":400,\n \"Msg\":\"NoneType event"\n\"Payload\":{}\n}'
-        ngx.say(json_str)
-        ngx.flush()
-        ngx.log(ngx.DEBUG,"recv post event failed")
-        return
-    end
-    local json_str = '{\n\"Code\":200,\n \"Msg\":\"Sucess"\n\"Payload\":{}\n}'
-    ngx.say(json_str)
-    ngx.flush()
-    ngx.log(ngx.DEBUG,"recv post event success")
-end
 
 local function event_error_handle(err)
 	ngx.log(ngx.ERR,"event_error_handle ERR: ", err)
 end
 
-local function event_handle(request_method)
-    if request_method == "GET" then
-        get_method()
-    elseif request_method == "POST" then
-        post_method()
-    elseif request_method == "PUT" then
-        put_method()
-    elseif request_method == "DELETE" then
-        delete_method()
-    end
+local function event_handle()
+    local request_method = ngx.var.request_method
+    ngx.req.read_body()
+    local request_body = ngx.req.get_body_data()
+    ngx.log(ngx.DEBUG,"recv event: ",request_method, request_body)
+
+    g_event_handle.add_handle(request_method, request_body)
+
+    local json_str = '{\n\"Code\":200,\n \"Msg\":\"Sucess"\n\"Payload\":{}\n}'
+    ngx.say(json_str)
+    ngx.flush()
 end
 -------main function------------
-local request_method = ngx.var.request_method
-local status = xpcall( event_handle, event_error_handle, request_method )
+local status = xpcall(event_handle, event_error_handle)
--- lua-func/init/init_worker.lua
+++ lua-func/init/init_worker.lua
@@ -4,6 +4,7 @@ local g_exec_heart = require("init.init_heartbeat")
 local g_event_report = require("event-func.event_report")
 local g_cmd_handle_model = require("cmd-func.cmd_handle_model")
 local g_rule_handle_model = require("rule-func.rule_handle_model")
+local g_event_handle_model = require("event-func.event_handle_model")
 local heart_time = 60
 local regular_report_time = 300
 local plat_report_time = 30
@@ -27,6 +28,13 @@ end
 local ok,err = ngx.timer.every(0.5, g_rule_handle_model.rule_handle_thread)
 if not ok then
     ngx.log(ngx.ERR,"rule handle thread, err:", err)
+    return
+end
+
+--event请求的异步处理
+local ok,err = ngx.timer.every(0.5, g_event_handle_model.event_handle_thread)
+if not ok then
+    ngx.log(ngx.ERR,"event handle thread, err:", err)
     return
 end
 
